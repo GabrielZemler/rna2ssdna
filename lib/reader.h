094605c00140bc490212ddeaad727b5c5caf2e15
GabrielZemler <131439113+GabrielZemler@users.noreply.github.com>
2024-03-07 10:51:25 +0000

    modified:   lib/reader.h 	new file:   src/5AHairpin.pdb 	new file:   src/dna.fasta 	modified:   src/main 	modified:   src/main.c

--- lib/reader.h
+++ lib/reader.h
@@ -1,33 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #ifndef reader_h
 #define reader_h
 #endif
-void read(char *inpt){
-FILE *ipt;
-char * line = NULL;
-size_t len = 0;
-size_t read;
-ipt=fopen(inpt,"r");
-if(ipt==NULL){
-    printf("----------------------------------------------------------\n");
-    printf("Fatal error: Unable to read from input file\n");
-    printf("----------------------------------------------------------\n");
-}
-else{
-    printf("Reading \n");
-    /*while ((read = getline(&line, &len, ipt)) != -1) {
-        printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
-    }*/
-    while (!feof(ipt)) {
-    char c[4]; char d;int n=0; int x=0; int p=0;
-    if (fscanf(ipt, "%*s %*d %*s %c %*c %*d %*f %*f %*f %*f %*f %*c",  &d) < 1)
-      break;
-    printf("%c\n", d);
- }
-}
+void read(char *inpt)
+{
+    FILE *ipt;
+    char * line = NULL;
+    size_t len = 0;
+    size_t read;
+    ipt=fopen(inpt,"r");
+    if(ipt==NULL)
+    {
+        printf("----------------------------------------------------------\n");
+        printf("Fatal error: Unable to read from input file\n");
+        printf("----------------------------------------------------------\n");
+        exit(1);
+    }
+    else
+    {
+        printf("Reading \n");
+        FILE *fasta;
+        int x=0;
+        fasta=fopen("dna.fasta", "wr");
+        if(fasta==NULL)
+        {
+        printf("----------------------------------------------------------\n");
+        printf("Fatal error: Unable to create output file dna.fasta\n");
+        printf("----------------------------------------------------------\n");
+        exit(1);
+        }
+        fprintf(fasta, ">sequence\n ; generated using rna2ssdna\n");
+        while (!feof(ipt)) 
+        {
+            char d;int n=1; char a[100]="ATOM";
+            char *ip;
+            ip=a;
+            if (fscanf(ipt, "%*s %*d %*s %c %*c %d %*f %*f %*f %*f %*f %*c",&d, &n) == 2){
+                    if(n>x)
+                    {
+                        fprintf(fasta,"%c", d );
+                        x++;
+                    }           
+                }
+                else if(fscanf(ipt,"%s%*[^\n]", a))
+                {
+                    printf("%s",a);
+                    if(strcmp(a,"TER")==0)
+                    {
+                        printf("ITS TER\n");
+                       
+                    }
+                }
+                else
+                {
+                    fscanf(ipt, "%*[^\n]\n");
+                    printf("called");
+                }
+        }
+        fclose(fasta);
+        fclose(ipt);
+    }
 }
 void flag(){
 
--- src/main.c
+++ src/main.c
@@ -1,35 +1,36 @@
 /* This is the main file of the programm which reads the user input and calls the needed functions in ./lib
 */
 #include<stdio.h>
+#include <stdlib.h>
 #include "../lib/reader.h"
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     printf("----------------------------------------------------------\n");
     printf("                WELLCOME TO RNA2ssDNA \n");
     printf("----------------------------------------------------------\n");
     int i;
-    for (i = 1; i < argc; i++) {
- 
-        /* Check for a switch (leading "-"). */
- 
-        if (argv[i][0] == '-') {
- 
-            /* Use the next character to decide what to do. */
- 
-            switch (argv[i][1]) {
+    for (i = 1; i < argc; i++) 
+    {
+        if (argv[i][0] == '-') 
+        {
+            switch (argv[i][1]) 
+            {
  
                 case 'f':
-                        if(argv[i+1]!=NULL){
-                        printf("Trying to read input from file %s\n", argv[i+1]);
-                        char *inpt=argv[i+1];
-                        read(inpt);
+                        if(argv[i+1]!=NULL)
+                        {
+                            printf("Trying to read input from file %s\n", argv[i+1]);
+                            char *inpt=argv[i+1];
+                            read(inpt);
                         }
-                        else{
+                        else
+                        {
                             printf("----------------------------------------------------------\n");
                             printf("Fatal error: Input file does not exist or is unaccesible\n");
                             printf("----------------------------------------------------------\n");
+                            exit(1);
                         }
                         break;
- 
                 case 'h':
                         printf("gerneric usage: rna2ssdna -f rna-file.pdb\n -o output.pdb:  declare output file (default: dna.pdb)\n -h:             call help\n");
                         break;
